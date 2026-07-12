1f83a343e95429505b294971992a45f89470c7e9
Ayush Jha <jhaa97204@gmail.com>
2026-07-12 21:41:21 +0530

    error fixed issue 8

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,3 @@
-"use client";
-
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.replace("/dashboard");
-  }, [router]);
-
-  return null;
+  return <div />;
 }
